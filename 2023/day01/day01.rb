0d8cf8c6cd8427f8ffb02a8a529911dc29e24abe
Den Patin <den@hey.com>
2023-12-03 16:53:15 +0200

    Solve day 1

--- 2023/day01/day01.rb
+++ 2023/day01/day01.rb
@@ -2,12 +2,33 @@
 
 class Day01
   def initialize(input)
-    @input = input
+    @input = input.split("\n")
   end
 
   def solve1
+    @input.sum { |line| number_from(line) }
   end
 
   def solve2
+    @input.sum { |line| number_from(convert_to_digits(line)) }
+  end
+
+  private
+
+  WORDS = %w[one two three four five six seven eight nine].freeze
+  DIGITS = WORDS.each_with_index.to_h { |w, i| [w, i + 1] }
+  DIGITS_REV = DIGITS.transform_keys(&:reverse)
+
+  def number_from(str)
+    digits = str.scan(/\d/)
+    "#{digits.first}#{digits.last}".to_i
+  end
+
+  def convert_to_digits(str)
+    pattern_beg = Regexp.union(DIGITS.keys)
+    pattern_end = Regexp.union(DIGITS_REV.keys)
+    str_beg = str.gsub(pattern_beg, DIGITS)
+    str_end = str.reverse.gsub(pattern_end, DIGITS_REV).reverse
+    str_beg + str_end
   end
 end
--- 2023/day01/test_day01.rb
+++ 2023/day01/test_day01.rb
@@ -4,23 +4,39 @@ require 'minitest/autorun'
 require_relative 'day01'
 
 class Day01Test < Minitest::Test
+  INPUT1 = <<~INPUT
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+  INPUT
+
+  INPUT2 = <<~INPUT
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    ninesixmlfjxhscninehqcdvxf8nzfivetwonehhd
+    4seven2psg8hqg9oneightnzc
+    zfourvxngsmmqsldpkrrbfnjf2mvkdhfs7eightwotc
+    2rzvpfpgzxk3863eightoneighttbb
+  INPUT
+
   def setup
-    input = <<~INPUT
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-    INPUT
-    @day01 = Day01.new(input)
+    @day011 = Day01.new(INPUT1)
+    @day012 = Day01.new(INPUT2)
   end
 
   def test_solve1
     expected = 142
-    assert_equal expected, @day01.solve1
+    assert_equal expected, @day011.solve1
   end
 
   def test_solve2
-    expected = nil
-    assert_equal expected, @day01.solve2
+    expected = 490
+    assert_equal expected, @day012.solve2
   end
 end
